9ea0deb9291736093dd0edfbbc62530ca2da8a01
Junya Hosokawa <hosokawa@morijyobi.com>
2016-07-01 08:30:29 +0900

    add png files
    gameicon_1～4.png を追加
    project.json を　変更　
    "showFPS" : false,　にするも　表示が消えない？？
    と考えたが、js liteでWebサーバを立ち上げなければ、project.jsonは読み込まれないので、FPSは消えない

--- src/myScene.js
+++ src/myScene.js
@@ -4,16 +4,17 @@ var MyLayer = cc.Layer.extend({
         this._super();
 
         var size = cc.director.getWinSize();
+/*
         var sprite = cc.Sprite.create(res.HelloWorld_png);
         sprite.setPosition(size.width / 2, size.height / 2);
         sprite.setScale(0.8);
         this.addChild(sprite, 0);
-
+*/
         var label = cc.LabelTTF.create("Hello World", "Arial", 40);
         label.setPosition(size.width / 2, size.height / 2);
         this.addChild(label, 1);
         //add code
-        var spriteB = cc.Sprite.create(res.Cocos_png);
+        var spriteB = cc.Sprite.create(res.gameicon_1_png);
        　spriteB.setPosition(size.width / 4, size.height / 4);
        　this.addChild(spriteB);
 
--- src/resource.js
+++ src/resource.js
@@ -2,6 +2,11 @@
 var res = {
     HelloWorld_png : "res/HelloWorld.png",
     Cocos_png : "res/cocos.png",
+    gameicon_1_png : "res/gameicon_1.png",
+    gameicon_2_png : "res/gameicon_2.png",
+    gameicon_3_png : "res/gameicon_3.png",
+    gameicon_4_png : "res/gameicon_4.png",
+    
 };
 
 var g_resources = [];
